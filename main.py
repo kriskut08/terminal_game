bf62bef8e65ad975afca2ddbd261a45d168fd419
kriskut08 <73740605+kriskut08@users.noreply.github.com>
2022-05-16 18:10:30 +0200

    weeeee majdnem kész

--- main.py
+++ main.py
@@ -2,28 +2,9 @@ from pynput import keyboard
 import os as os
 import json
 
-
-#board = [['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '#', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 
-#        ['-', '-', '-', '-', '-', '-', '-', '-', '-', '#']]
-
-
-#open tabla.json and load it's content into a variable
 f = open("tabla.json")
 l = json.load(f)
 
-board = []
-
-for i in l['tabla']:
-	s = i.split(' ')
-	board.append(s)
 
 
 
@@ -49,40 +30,76 @@ def on_release(key):
 	move(key)
 	printboard(board)
 	if key == keyboard.Key.esc:
-        # Stop listener
 		return False
 
-
+jelek = ["#","C"]
+tabla = -1
 x,y = int(l["kezdes"][0]),int(l["kezdes"][2])
+
+def load_board():
+	global board
+	global tabla
+	global l
+	global x
+	global y
+	x,y = int(l["kezdes"][0]),int(l["kezdes"][2])
+	tabla += 1
+	print(tabla)
+	board = []
+	try:
+		for i in l['tabla{0}'.format(str(tabla))]:
+			s = i.split(' ')
+			board.append(s)
+	except:
+		print(l["exit_message"])
+		quit()		
+	board[x][y] = 'X'
+
 
 def move(key):
 	global x,y
 	if key == keyboard.Key.left or key == 'a':
-		if y>0 and board[x][y-1] != '#':
+		if y>0 and board[x][y-1] not in jelek:
 			board[x][y]= '-'
 			y-=1
 			board[x][y] = 'X'
+		else:
+			if board[x][y-1] == 'C':
+				load_board()
 	if key == keyboard.Key.right or key == 'd':
-		if y<9 and board[x][y+1] != '#':
+		if y<9 and board[x][y+1] not in jelek:
 			board[x][y]= '-'
 			y+=1
 			board[x][y] = 'X'
+		else:
+			if board[x][y+1] == 'C':
+				load_board()
 	if key == keyboard.Key.up or key == 'w':
-		if x>0 and board[x-1][y] != '#':
+		if x>0 and board[x-1][y] not in jelek:
 			board[x][y]= '-'
 			x-=1
 			board[x][y] = 'X'
+		else:
+			if board[x-1][y] == 'C':
+				load_board()
 	if key == keyboard.Key.down or key == 's':
-		if x<9 and board[x+1][y] != '#':
+		if x<9 and board[x+1][y] not in jelek:
 			board[x][y]= '-'
 			x+=1
 			board[x][y] = 'X'
+		else:
+			if board[x+1][y] == 'C':
+				load_board()
 	
 #a játék kezdete:
-clear() 
-print(l["message"])
-board[x][y] = 'X'
-printboard(board)
+def start():
+	load_board()
+	clear() 
+	print(l["message"])
+	board[x][y] = 'X'
+	printboard(board)
 
-with keyboard.Listener(on_release=on_release) as listener:
-    listener.join()
+	with keyboard.Listener(on_release=on_release) as listener:
+		listener.join()
+if tabla==-1:
+    start()
